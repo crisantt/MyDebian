5b93335d6a4e35167d217c0063d3879316b5c77e
Cris <152991141+crisantt@users.noreply.github.com>
2025-02-08 02:34:54 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo apt install build-essential xorg xinit x11-server-utils libx11-dev xorg-dev xserver-xorg-video-amdgpu pkg-config clangd make curl wget -y
+sudo apt install build-essential xorg xinit x11-server-utils \
+libx11-dev xorg-dev xserver-xorg-video-amdgpu pkg-config \
+clangd make curl wget -y
