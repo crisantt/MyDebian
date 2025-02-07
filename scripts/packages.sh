aec8c6b27ecd6f2b1ab535e8a6b9cb4d3ecfd634
Cris <152991141+crisantt@users.noreply.github.com>
2025-02-08 02:37:58 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-sudo apt install build-essential xorg xinit x11-server-utils \
+sudo apt install build-essential xorg xinit x11-xserver-utils \
 libx11-dev xorg-dev xserver-xorg-video-amdgpu pkg-config \
 clangd make curl wget -y
+
+#AMD Mesa Drivers
+sudo dpkg --add-architecture i386
+sudo add-apt-repository ppa:kisak/kisak-mesa -y
+sudo apt update
+sudo apt install libgl1-mesa-dri:i386 mesa-vulkan-drivers mesa-vulkan-drivers:i386 -y
