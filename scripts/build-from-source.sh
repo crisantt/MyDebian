de05047befe59eb9d06d55246ad276417a2193ab
Cris <152991141+crisantt@users.noreply.github.com>
2025-02-08 02:26:09 +0800

    Update build-from-source.sh

--- scripts/build-from-source.sh
+++ scripts/build-from-source.sh
@@ -50,3 +50,18 @@ echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https:/
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 
 sudo apt update && sudo apt install codium -y
+
+#Zen-Browser (2025-feb-05)
+cd ~/packages
+wget https://github.com/zen-browser/desktop/releases/download/1.7.4b/zen.linux-x86_64.tar.bz2
+tar -zvjf zen.linux-x86_64.tar.bz2
+echo "export PATH=\$PATH:\$HOME/packages/zen" >> $HOME/.bashrc
+source .bashrc
+sudo ln -sf $HOME/packages/zen/zen-bin /usr/local/bin/zen
+
+#Fastfetch (2025-feb-05)
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.35.0/fastfetch-linux-amd64.deb
+sudo apt install ./fastfetch-linux-amd64.deb -y
+
+cd ~/
+echo "DONEEE!!"
